dd80bcf111f850994b1a7e3494eca5f88eadfd5a
rafi <muhammadrafishidiq@gmail.com>
2023-05-19 00:30:41 +0700

    RollBack 1

--- app.py
+++ app.py
@@ -1,11 +1,194 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from dbconnect import get_mysql_connection
-from routes import configure_routes
 
 app = Flask(__name__)
-
 db_connection = get_mysql_connection()
-configure_routes(app, db_connection)
+
+# get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM users')
+        rows = cursor.fetchall()
+        cursor.close()
+        response = {'error': False, 'message': 'success', 'count': len(rows), 'users': rows}
+        return jsonify(response), 200
+    except Exception as e:
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# get user by id
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    try:
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM users WHERE id=%s', (id,))
+        row = cursor.fetchone()
+        cursor.close()
+        if not row:
+            response = {'error': True, 'message': 'User not found'}
+            return jsonify(response), 404
+        response = {'error': False, 'message': 'success', 'user': row}
+        return jsonify(response), 200
+    except Exception as e:
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# create new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        data = request.json
+        if not data or not all(key in data for key in ['Username', 'Email', 'Password', 'Gender', 'BirthDate', 'Height', 'Weight', 'FatNeed', 'ProteinNeed', 'CaloryNeed', 'FiberNeed', 'CarbohidrateNeed']):
+            return 'Missing fields', 400
+        cursor = db_connection.cursor()
+        query = 'INSERT INTO users (Username, Email, Password, Gender, BirthDate, Height, Weight, FatNeed, ProteinNeed, CaloryNeed, FiberNeed, CarbohidrateNeed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        values = (data['Username'], data['Email'], data['Password'], data['Gender'], data['BirthDate'], data['Height'], data['Weight'], data['FatNeed'], data['ProteinNeed'], data['CaloryNeed'], data['FiberNeed'], data['CarbohidrateNeed'])
+        cursor.execute(query, values)
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'User created'}
+        return jsonify(response), 201
+    except Exception as e:
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# update existing user
+@app.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    try:
+        data = request.json
+        if not data:
+            return 'Missing fields', 400
+        cursor = db_connection.cursor()
+        query = 'UPDATE users SET '
+        values = []
+        for key in data:
+            query += key + '=%s, '
+            values.append(data[key])
+        query = query[:-2]   # remove last comma and space
+        query += ' WHERE id=%s'
+        values.append(id)
+        cursor.execute(query, values)
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'User updated'}
+        return jsonify(response), 200
+    except Exception as e:
+        db_connection.rollback()
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# delete user
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute('DELETE FROM users WHERE id=%s', (id,))
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'User deleted'}
+        return jsonify(response), 200
+    except Exception as e:
+        db_connection.rollback()
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+
+# get all intake users
+@app.route('/intake_users', methods=['GET'])
+def get_intake_users():
+    try:
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM intake_users')
+        rows = cursor.fetchall()
+        cursor.close()
+        response = {'error': False, 'message': 'success', 'count': len(rows), 'intake_users': rows}
+        return jsonify(response), 200
+    except Exception as e:
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# get intake user by id
+@app.route('/intake_users/<int:id>', methods=['GET'])
+def get_intake_user(id):
+    try:
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM intake_users WHERE id=%s', (id,))
+        row = cursor.fetchone()
+        cursor.close()
+        if not row:
+            response = {'error': True, 'message': 'Intake User not found'}
+            return jsonify(response), 404
+        response = {'error': False, 'message': 'success', 'intake_user': row}
+        return jsonify(response), 200
+    except Exception as e:
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# create new intake user
+@app.route('/intake_users', methods=['POST'])
+def create_intake_user():
+    try:
+        data = request.json
+        if not data or not all(key in data for key in ['user_id', 'HealthStatus', 'FatIntake', 'CaloryIntake', 'FiberIntake', 'CarbohidrateIntake', 'Feedback']):
+            response = {'error': True, 'message': 'Missing fields'}
+            return jsonify(response), 400
+        cursor = db_connection.cursor()
+        query = 'INSERT INTO intake_users (user_id, HealthStatus, FatIntake, CaloryIntake, FiberIntake, CarbohidrateIntake, Feedback) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+        values = (data['user_id'], data['HealthStatus'], data['FatIntake'], data['CaloryIntake'], data['FiberIntake'], data['CarbohidrateIntake'], data['Feedback'])
+        cursor.execute(query, values)
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'Intake user created'}
+        return jsonify(response), 201
+    except Exception as e:
+        db_connection.rollback()
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# update existing intake user
+@app.route('/intake_users/<int:id>', methods=['PUT'])
+def update_intake_user(id):
+    try:
+        data = request.json
+        if not data:
+            response = {'error': True, 'message': 'Missing fields'}
+            return jsonify(response), 400
+        cursor = db_connection.cursor()
+        query = 'UPDATE intake_users SET '
+        values = []
+        for key in data:
+            query += key + '=%s, '
+            values.append(data[key])
+        query = query[:-2]   # remove last comma and space
+        query += ' WHERE id=%s'
+        values.append(id)
+        cursor.execute(query, values)
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'Intake user updated'}
+        return jsonify(response), 200
+    except Exception as e:
+        db_connection.rollback()
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
+
+# delete intake user
+@app.route('/intake_users/<int:id>', methods=['DELETE'])
+def delete_intake_user(id):
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute('DELETE FROM intake_users WHERE id=%s', (id,))
+        db_connection.commit()
+        cursor.close()
+        response = {'error': False, 'message': 'Intake user deleted'}
+        return jsonify(response), 200
+    except Exception as e:
+        db_connection.rollback()
+        response = {'error': True, 'message': str(e)}
+        return jsonify(response), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- nutriast.sql
+++ nutriast.sql
@@ -11,17 +11,17 @@
  Target Server Version : 100418
  File Encoding         : 65001
 
- Date: 18/05/2023 12:47:45
+ Date: 19/05/2023 00:25:23
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for intake
+-- Table structure for intake_users
 -- ----------------------------
 DROP TABLE IF EXISTS `intake_users`;
-CREATE TABLE `intake`  (
+CREATE TABLE `intake_users`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `HealthStatus` char(16) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
@@ -34,11 +34,12 @@ CREATE TABLE `intake`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `UserIntake`(`user_id` ASC) USING BTREE,
   CONSTRAINT `UserIntake` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of intake
+-- Records of intake_users
 -- ----------------------------
+INSERT INTO `intake_users` VALUES (1, 15, 'good', 200, 120, 2000, 40, 200, NULL);
 
 -- ----------------------------
 -- Table structure for users
@@ -46,22 +47,25 @@ CREATE TABLE `intake`  (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `gender` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `Username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `Email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `Password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `BirthDate` date NOT NULL,
+  `Gender` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `Height` int NOT NULL,
-  `weight` int NOT NULL,
+  `Weight` int NOT NULL,
   `FatNeed` int NULL DEFAULT NULL,
   `ProteinNeed` int NULL DEFAULT NULL,
   `CaloryNeed` int NULL DEFAULT NULL,
   `FiberNeed` int NULL DEFAULT NULL,
+  `CarbohidrateNeed` int NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
+INSERT INTO `users` VALUES (15, 'Slvally', 'kyurem@gmail.com', 'admin', '2011-04-05', 'male', 169, 45, 0, 0, 0, 0, 0);
+INSERT INTO `users` VALUES (16, 'JohnDoe', 'johndoe@example.com', 'password', '1990-01-01', 'Male', 180, 75, 200, 120, 2000, 40, 200);
 
 SET FOREIGN_KEY_CHECKS = 1;
